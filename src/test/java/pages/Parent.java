8e7afd7917ae1b34ac242dbac9a6073a3d263e41
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-04-06 17:13:25 +0300

    Reorganized code layout

--- src/test/java/pages/Parent.java
+++ src/test/java/pages/Parent.java
@@ -16,54 +16,65 @@ import java.util.List;
 public class Parent {
     public WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(5));
 
-    public void myClick(WebElement element){
+    public void myClick(WebElement element) {
         wait.until(ExpectedConditions.elementToBeClickable(element));
         scrollToElement(element);
         element.click();
     }
+
     public void mySendKeys(WebElement element, String text) {
         wait.until(ExpectedConditions.visibilityOf(element));
         scrollToElement(element);
         element.clear();
         element.sendKeys(text);
     }
-    public void scrollToElement(WebElement element){
-        JavascriptExecutor js=(JavascriptExecutor) GWD.getDriver();
+
+    public void scrollToElement(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
         js.executeScript("arguments[0].scrollIntoView();", element);
     }
-    public void verifyContainsText(WebElement element, String value){
-        wait.until(ExpectedConditions.textToBePresentInElement(element,value));
+
+    public void verifyContainsText(WebElement element, String value) {
+        wait.until(ExpectedConditions.textToBePresentInElement(element, value));
         Assert.assertTrue(element.getText().toLowerCase().contains(value.toLowerCase()));
         new Actions(GWD.getDriver()).sendKeys(Keys.ESCAPE).build().perform();
     }
+
     public void myJsClick(WebElement element) {
         wait.until(ExpectedConditions.elementToBeClickable(element));
         JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
         js.executeScript("arguments[0].click();", element);
     }
+
     public void selectByIndex(WebElement element, int num) {
         wait.until(ExpectedConditions.visibilityOf(element));
         Select menu = new Select(element);
         menu.selectByIndex(num);
     }
+
     public void selectByText(WebElement element, String text) {
         wait.until(ExpectedConditions.visibilityOf(element));
         Select select = new Select(element);
         select.selectByVisibleText(text);
     }
-    public void hoverOver(WebElement element){
+
+    public void hoverOver(WebElement element) {
         wait.until(ExpectedConditions.visibilityOf(element));
         new Actions(GWD.getDriver()).moveToElement(element).build().perform();
     }
+
     public void waitUntilVisibilityOf(WebElement element) {
         wait.until(ExpectedConditions.visibilityOf(element));
     }
+
     public void waitUntilElementToBeClickable(WebElement element) {
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
+
     public static int randomGenerator(int range) {
         return (int) (Math.random() * range);
     }
+
     public static boolean listContainsString(List<WebElement> list, String search) {
         boolean isFound = false;
         for (WebElement e : list) {
@@ -72,6 +83,7 @@ public class Parent {
         }
         return isFound;
     }
+
     public static boolean compareLists(List<WebElement> list1, List<String> list2) {
         // Check if the lists are of equal size
         if (list1.size() != list2.size()) {
@@ -86,6 +98,7 @@ public class Parent {
         }
         return true;
     }
+
     public void wait(int sn) {
         try {
             Thread.sleep(sn * 1000);
