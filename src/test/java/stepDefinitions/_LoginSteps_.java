a2340dc1729aaf0b38e54101c7cb3bec6a6683bc
Artam <erdemmmgurel@gmail.com>
2024-04-08 16:28:01 +0300

    US_601_AccountCreating.feature and step class created

---
+++ src/test/java/stepDefinitions/_LoginSteps_.java
@@ -0,0 +1,24 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _LoginSteps_ {
+    @Given("Navigate to Parabank website")
+    public void navigateToParabankWebsite() {
+    }
+
+    @When("Click on the Register button")
+    public void clickOnTheRegisterButton() {
+    }
+
+    @Then("Fill the registration information")
+    public void fillTheRegistrationInformation() {
+    }
+
+    @And("Click on the Register Submit button")
+    public void clickOnTheRegisterSubmitButton() {
+    }
+}
