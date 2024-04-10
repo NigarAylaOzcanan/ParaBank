a151415b1167060b029192bdbdd7d1369a95aef0
Mednasa <girgin183@gmail.com>
2024-04-10 15:03:26 +0300

    US602_ Done

--- src/test/java/stepDefinitions/_LoginSteps_.java
+++ src/test/java/stepDefinitions/_LoginSteps_.java
@@ -1,16 +1,37 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import pages.Parent;
+import pages.US602_Content;
 import pages.US606_Content;
 
 public class _LoginSteps_ extends Parent {
     US606_Content us6 = new US606_Content();
+    US602_Content us2=new US602_Content();
 
     @Then("Enter username and password and click login button")
     public void enterUsernameAndPasswordAndClickLoginButton() {
         mySendKeys(us6.usernamePlc, "weagfdg");
         mySendKeys(us6.passwordPlc, "T4rg8H!Kp6i7b");
         myClick(us6.loginBtn);
+    }
+
+
+    @When("Enter username is {string} password as {string}")
+    public void enterUsernameIsPasswordAs(String username, String password) {
+        mySendKeys(us6.usernamePlc,username);
+        mySendKeys(us6.passwordPlc,password);
+        myClick(us6.loginBtn);
+    }
+
+    @When("Enter log out button")
+    public void enterLogOutButton() {
+        myClick(us2.logOutBtn);
+    }
+
+    @Then("Failed message should be displayed")
+    public void failedMessageShouldBeDisplayed() {
+        verifyContainsText(us2.failedMessage, "username");
     }
 }
