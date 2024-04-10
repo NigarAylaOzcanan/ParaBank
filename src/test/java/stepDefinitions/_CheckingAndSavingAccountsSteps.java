22d071274ee2bc12a1426903bbfa877241510ef8
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-04-10 15:46:12 +0300

    Added step class for US_604

---
+++ src/test/java/stepDefinitions/_CheckingAndSavingAccountsSteps.java
@@ -0,0 +1,35 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _CheckingAndSavingAccountsSteps {
+    @When("From the menu on the website, click the {string} link")
+    public void fromTheMenuOnTheWebsiteClickTheLink(String arg0) {
+    }
+
+    @And("The user is directed to the bank account creating page")
+    public void theUserIsDirectedToTheBankAccountCreatingPage() {
+    }
+
+    @And("The user selects the account  type from the Drop-Down menu as {string}")
+    public void theUserSelectsTheAccountTypeFromTheDropDownMenuAs(String arg0) {
+    }
+
+    @And("The user sees a warning that the new bank account should have a minimum balance")
+    public void theUserSeesAWarningThatTheNewBankAccountShouldHaveAMinimumBalance() {
+    }
+
+    @And("The user selects the account from the Drop-Down menu")
+    public void theUserSelectsTheAccountFromTheDropDownMenu() {
+    }
+
+    @And("The user clicks the on {string} button")
+    public void theUserClicksTheOnButton(String arg0) {
+    }
+
+    @Then("The user confirms the successful creation of the new bank account")
+    public void theUserConfirmsTheSuccessfulCreationOfTheNewBankAccount() {
+    }
+}
