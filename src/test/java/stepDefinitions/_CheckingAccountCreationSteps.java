059a6372e024b291dde974d643bec1b1a7b82dae
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-04-10 21:06:27 +0300

    Refactored step class

--- src/test/java/stepDefinitions/_CheckingAccountCreationSteps.java
+++ src/test/java/stepDefinitions/_CheckingAccountCreationSteps.java
@@ -23,30 +23,26 @@ public class _CheckingAccountCreationSteps extends Parent {
         for (int i = 0; i < linkList.size(); i++) {
             WebElement linkWebElement = cnt.getWebElement(linkList.get(i));
             cnt.myClick(linkWebElement);
-
         }
     }
-
     @And("The user is directed to the bank account creating page")
     public void theUserIsDirectedToTheBankAccountCreatingPage() {
-
         String currentUrl= GWD.getDriver().getCurrentUrl();
         String expectedUrl = "https://parabank.parasoft.com/parabank/openaccount.htm";
         wait.until(ExpectedConditions.urlToBe("https://parabank.parasoft.com/parabank/openaccount.htm"));
         Assert.assertEquals(currentUrl,expectedUrl, "URL does not match!");
-
     }
-    @And("The user selects the account type from the Drop-Down menu")
-    public void theUserSelectsTheAccountTypeFromTheDropDownMenu() {
-        cnt.selectByText(cnt.selectAccountType, "CHECKING");
+    @And("The user selects the account type as {string} from the Drop-Down menu")
+    public void theUserSelectsTheAccountTypeAsFromTheDropDownMenu(String accountType) {
+        cnt.selectByText(cnt.selectAccountType,accountType);
     }
     @And("The user sees a warning that the new bank account should have a minimum balance")
     public void theUserSeesAWarningThatTheNewBankAccountShouldHaveAMinimumBalance() {
         cnt.waitUntilVisibilityOf(cnt.warningMessage);
         System.out.println(cnt.warningMessage.getText());
     }
-    @And("The user selects the account of the {int} from Drop-Down menu")
-    public void theUserSelectsTheAccountOfTheDropDownMenu(int index) {
+    @And("The user selects the account of {int} the from Drop-Down menu")
+    public void theUserSelectsTheAccountOfIndexTheFromDropDownMenu(int index) {
         cnt.selectByIndex(cnt.selectAccount,index);
     }
     @Then("The user confirms the successful creation of the new bank account")
@@ -54,5 +50,4 @@ public class _CheckingAccountCreationSteps extends Parent {
         cnt.waitUntilVisibilityOf(cnt.successMsg);
         System.out.println(cnt.successMsg.getText());
     }
-
 }
