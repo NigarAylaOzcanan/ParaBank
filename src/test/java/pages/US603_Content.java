8b09a09bc7fa52465b477a4cc3b6e9ccd89aeab6
Mednasa <girgin183@gmail.com>
2024-04-12 00:28:50 +0300

    US603_ classes and feature added

---
+++ src/test/java/pages/US603_Content.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.GWD;
+
+import java.util.List;
+
+public class US603_Content extends Parent{
+
+    public US603_Content() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(css = "tbody > tr input")
+    public List<WebElement> payeeInformation;
+
+    @FindBy(css="[type='submit']")
+    public WebElement sendPaymentCl;
+
+
+
+
+}
