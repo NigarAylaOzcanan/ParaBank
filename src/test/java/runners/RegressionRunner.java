09e112fb57c340c8d38373b2e0cdb97140131db4
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-04-12 11:46:36 +0300

    Added group tags

--- src/test/java/runners/RegressionRunner.java
+++ src/test/java/runners/RegressionRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "Regression",
+        tags = "@Regression",
         features = {"src/test/java/featureFiles"},
         glue = {"stepDefinitions"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
--- src/test/java/runners/SmokeRunner.java
+++ src/test/java/runners/SmokeRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "Smoke",
+        tags = "@Smoke",
         features = {"src/test/java/featureFiles"},
         glue = {"stepDefinitions"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
