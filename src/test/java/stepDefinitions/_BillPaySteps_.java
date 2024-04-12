ab39220f76196418c06bc00e9d6690afed1f7751
Mednasa <girgin183@gmail.com>
2024-04-12 10:23:25 +0300

    US603_ elements and methods added

--- src/test/java/stepDefinitions/_BillPaySteps_.java
+++ src/test/java/stepDefinitions/_BillPaySteps_.java
@@ -10,10 +10,8 @@ public class _BillPaySteps_ extends Parent {
     US603_Content us3=new US603_Content();
     @When("Click on the Bill Pay Button")
     public void clickOnTheBillPayButton() {
-
+        myClick(us3.billPayBtn);
     }
-
-
 
     @And("Click on the Send Payment button")
     public void clickOnTheSendPaymentButton() {
@@ -35,31 +33,32 @@ public class _BillPaySteps_ extends Parent {
 
     @And("Click on the Accounts Overview button")
     public void clickOnTheAccountsOverviewButton() {
+        myClick(us3.accountsOverviewBtn);
     }
 
     @Then("Fill the payee Water information")
     public void fillThePayeeWaterInformation() {
-        mySendKeys(us3.payeeInformation.get(0),"EnerjiSA Elektrik Faturasi");
-        mySendKeys(us3.payeeInformation.get(1),"Uzum Sk:26");
-        mySendKeys(us3.payeeInformation.get(2),"Sariyer");
+        mySendKeys(us3.payeeInformation.get(0),"ISKI Su Faturasi");
+        mySendKeys(us3.payeeInformation.get(1),"Bulbul Sk. No:19");
+        mySendKeys(us3.payeeInformation.get(2),"Tarabya");
         mySendKeys(us3.payeeInformation.get(3),"Istanbul");
-        mySendKeys(us3.payeeInformation.get(4),"34098");
+        mySendKeys(us3.payeeInformation.get(4),"34440");
         mySendKeys(us3.payeeInformation.get(5),"5551234545");
-        mySendKeys(us3.payeeInformation.get(6),"1122");
-        mySendKeys(us3.payeeInformation.get(7),"1122");
-        mySendKeys(us3.payeeInformation.get(8),"85");
+        mySendKeys(us3.payeeInformation.get(6),"3344");
+        mySendKeys(us3.payeeInformation.get(7),"3344");
+        mySendKeys(us3.payeeInformation.get(8),"45");
     }
 
     @Then("Fill the payee Natural Gas invoice information")
     public void fillThePayeeNaturalGasInvoiceInformation() {
-        mySendKeys(us3.payeeInformation.get(0),"EnerjiSA Elektrik Faturasi");
-        mySendKeys(us3.payeeInformation.get(1),"Uzum Sk:26");
-        mySendKeys(us3.payeeInformation.get(2),"Sariyer");
+        mySendKeys(us3.payeeInformation.get(0),"IGDAS Dogalgaz Faturasi");
+        mySendKeys(us3.payeeInformation.get(1),"Caycı Sk.No:13");
+        mySendKeys(us3.payeeInformation.get(2),"Besiktas");
         mySendKeys(us3.payeeInformation.get(3),"Istanbul");
-        mySendKeys(us3.payeeInformation.get(4),"34098");
+        mySendKeys(us3.payeeInformation.get(4),"34555");
         mySendKeys(us3.payeeInformation.get(5),"5551234545");
-        mySendKeys(us3.payeeInformation.get(6),"1122");
-        mySendKeys(us3.payeeInformation.get(7),"1122");
-        mySendKeys(us3.payeeInformation.get(8),"85");
+        mySendKeys(us3.payeeInformation.get(6),"5656");
+        mySendKeys(us3.payeeInformation.get(7),"5656");
+        mySendKeys(us3.payeeInformation.get(8),"120");
     }
 }
